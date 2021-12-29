7ef8b79c35fcb948b190e46d37008ed05ef176d4
Eduardo Campaña <asrielo@gmail.com>
2021-12-29 13:15:53 +0900

    adds delete update get user controllers

--- server/src/controllers/users.rs
+++ server/src/controllers/users.rs
@@ -33,14 +33,64 @@ impl User {
     }
 
     pub async fn get_user(req: tide::Request<State>) -> tide::Result {
-        unimplemented!()
+        let id = req.param("id")?;
+        let db = req.state().db.clone();
+
+        let user: Option<User> = sqlx::query_as(
+            "
+            SELECT * FROM users
+            WHERE id = $1
+            ",
+        )
+        .bind(id)
+        .fetch_optional(&db)
+        .await?;
+
+        let mut res = tide::Response::new(200);
+        res.set_body(tide::Body::from_json(&user)?);
+        Ok(res)
     }
 
-    pub async fn update_user(req: tide::Request<State>) -> tide::Result {
-        unimplemented!()
+    pub async fn update_user(mut req: tide::Request<State>) -> tide::Result {
+        let payload: User = req.body_json().await?;
+        let id = req.param("id")?;
+        let db = req.state().db.clone();
+
+        let user: Option<User> = sqlx::query_as(
+            "
+            UPDATE users
+            SET username = $1, password = $2
+            WHERE id = $3
+            RETURNING id, username, password
+            ",
+        )
+        .bind(payload.username)
+        .bind(payload.password)
+        .bind(id)
+        .fetch_optional(&db)
+        .await?;
+
+        let mut res = tide::Response::new(200);
+        res.set_body(tide::Body::from_json(&user)?);
+        Ok(res)
     }
     pub async fn delete_user(req: tide::Request<State>) -> tide::Result {
-        unimplemented!()
+        let id = req.param("id")?;
+        let db = req.state().db.clone();
+
+        let user: Option<User> = sqlx::query_as(
+            "
+            DELETE FROM users
+            WHERE id = $1
+            ",
+        )
+        .bind(id)
+        .fetch_optional(&db)
+        .await?;
+
+        let mut res = tide::Response::new(204);
+        res.set_body(tide::Body::from_json(&user)?);
+        Ok(res)
     }
 
     pub async fn list_users(req: tide::Request<State>) -> tide::Result {
